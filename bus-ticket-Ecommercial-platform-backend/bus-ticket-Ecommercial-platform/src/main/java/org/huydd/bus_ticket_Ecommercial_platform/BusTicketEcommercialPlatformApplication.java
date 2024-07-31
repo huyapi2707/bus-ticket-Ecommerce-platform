9ead6bda30bbe67a77885875701045b8d017162c
huydangapi2707 <dangdinhhuyisme@gmail.com>
2024-07-31 22:32:04 +0700

    init project

---
+++ bus-ticket-Ecommercial-platform-backend/bus-ticket-Ecommercial-platform/src/main/java/org/huydd/bus_ticket_Ecommercial_platform/BusTicketEcommercialPlatformApplication.java
@@ -0,0 +1,13 @@
+package org.huydd.bus_ticket_Ecommercial_platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BusTicketEcommercialPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BusTicketEcommercialPlatformApplication.class, args);
+	}
+
+}
---
+++ bus-ticket-Ecommercial-platform-backend/bus-ticket-Ecommercial-platform/src/test/java/org/huydd/bus_ticket_Ecommercial_platform/BusTicketEcommercialPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package org.huydd.bus_ticket_Ecommercial_platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BusTicketEcommercialPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
